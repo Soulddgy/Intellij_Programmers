3006c3a84d69a125536cdb7745eda0f99171a55d
Soulddgy <rlawkthfwk@gmail.com>
2023-12-16 09:45:46 +0900

    #프로그래머스에 있는 문제
    2023-12-16
    #summer winter coding 2019
    -ex1 (멀쩡한 사각형) : 내가 푼 코딩은 자료형의 문제로
    몇몇 케이스만 맞고 그치만, 테스트케이스가 별로 안주어지고 문제가 살짝 이상함;;
    
    #practice
    -ex1 (달리기 경주) : level 1 짜리 hashmap으로 풀었음

---
+++ src/practice/ex1.java
@@ -0,0 +1,22 @@
+package practice;
+//연습문제중 "달리기 경주" 문제
+//level1
+import java.util.*;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> map = new HashMap<>();
+        String[] answer = players;
+        for(int i=0;i<players.length;i++){
+            map.put(players[i], i);
+        }
+        for(int i=0;i<callings.length;i++){
+            int count = map.get(callings[i]);
+            String prePlayer = answer[count-1];
+            answer[count-1] = callings[i];
+            answer[count] = prePlayer;
+            map.put(callings[i], count-1);
+            map.put(prePlayer, count);
+        }
+        return answer;
+    }
+}
